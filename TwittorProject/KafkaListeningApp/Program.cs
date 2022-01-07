f8799b391f7698afbd148cbc4def01b59bbc9665
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-07 16:10:31 +0700

    Init Project Twittor

---
+++ TwittorProject/KafkaListeningApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaListeningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
