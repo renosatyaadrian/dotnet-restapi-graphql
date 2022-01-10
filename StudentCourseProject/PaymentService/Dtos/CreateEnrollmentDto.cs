755c7855fe565ec2516ba4f36c72a53c8331359e
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-10 16:36:34 +0700

    add enrollment dtos

---
+++ StudentCourseProject/PaymentService/Dtos/CreateEnrollmentDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService.Dtos
+{
+    public class CreateEnrollmentDto
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int Grade { get; set; }
+    }
+}
