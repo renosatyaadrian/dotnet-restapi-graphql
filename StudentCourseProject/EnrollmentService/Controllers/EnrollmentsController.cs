e5cb1de5f2af7a41b1e563443bb8691c1ffc80e9
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-10 14:00:32 +0700

    fix naming

--- StudentCourseProject/EnrollmentService/Controllers/EnrollmentsController.cs
+++ StudentCourseProject/EnrollmentService/Controllers/EnrollmentsController.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using EnrollmentService.Data;
 using EnrollmentService.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnrollmentService.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [ApiController]
     [Route("api/[controller]")]
-    public class EnrollmensController : ControllerBase
+    public class EnrollmentsController : ControllerBase
     {
         private readonly IEnrollment _enrollment;
 
-        public EnrollmensController(IEnrollment enrollment)
+        public EnrollmentsController(IEnrollment enrollment)
         {
             _enrollment = enrollment;
         }
