e3d45565ed2f1d4b837c249dd67137ea2fb8579a
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-10 14:43:33 +0700

    add models from ef scaffold

---
+++ StudentCourseProject/PaymentService/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PaymentService.Models
+{
+    public partial class Enrollment
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public int Studentid { get; set; }
+        public int Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
