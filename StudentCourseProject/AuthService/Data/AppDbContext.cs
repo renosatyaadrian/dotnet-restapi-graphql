0e6e987c84022d7b35c07de1232be7e4e7231c72
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-11 16:03:50 +0700

    fix db context

---
+++ StudentCourseProject/AuthService/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+    }
+}
---
+++ StudentCourseProject/AuthService/Data/AppSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Data
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
